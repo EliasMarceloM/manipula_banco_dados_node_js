b174cee8ff756bb5fdc4867949dc534c0e310892
EliasMarceloM <106849054+EliasMarceloM@users.noreply.github.com>
2022-07-02 17:23:44 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var acao = 0
 var listaUsuraios
 var nome=""
 var senha=""
+var acesso=0
 //-- para usar body-parser
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -32,7 +33,7 @@ con.connect(function(err) {
     pNome="'"+pNome+"'"
     pSenha="'"+pSenha+"'"
     var sql = "INSERT INTO usuarios (nome, senha) VALUES "+"("+ pNome+","+pSenha+")";
-   // console.log(sqlx);
+   
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("foi inserido um úsurio "); 
@@ -45,7 +46,7 @@ con.connect(function(err) {
 //------------grava os dados 
 
 //------------------------ acessa os dados 
- /**/
+ 
 
 function buscaUsuario(pNome,pSenha) {
 con.connect(function(err) {
@@ -54,26 +55,33 @@ con.connect(function(err) {
     if (err) throw err;
     //console.log(result);
     listaUsuraios=result
-   // console.log(typeof (listaUsuraios))
+   
 //---mostra todos os nomes 
-    
+    acesso=1
    for (const property in listaUsuraios) {
   console.log( listaUsuraios[property].nome);
   console.log( property);
+   
     if(listaUsuraios[property].nome == pNome && listaUsuraios[property].senha == pSenha) {
 	 console.log("nome e senha corretos" ); 
-	   
+	 acesso=2
+	  
     }else {
-    console.log("nome ou senha não corretos" );
+    	
+    	if (acesso==1) {
+    	console.log("nome ou senha não corretos" );
+    	}
     }//fimelse
-    
+    console.log("o acesso é igual a: "+acesso);
 }
 //---mostra todos os nomes 
   });
 }); 
+return acesso 
 }
 
 //------------------------ acessa os dados 
+
 //------------conexão com banco de dados 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -86,12 +94,29 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/rlogin', (req, res) => {
-  res.sendFile(__dirname + '/rlogin.html')
-  res.send('nome:'+req.body.nome+'<br/>'+' senha:'+req.body.senha)
+  //res.sendFile(__dirname + '/rlogin.html')
+  //res.send('nome:'+req.body.nome+'<br/>'+' senha:'+req.body.senha)
   nome=req.body.nome // vou madar nome e senha como paramentro para a função que verifica 
   senha=req.body.senha// vou madar nome e senha como paramentro para a função que verifica 
-  console.log(nome)
   buscaUsuario(nome,senha)
+  //espera a função buscaUsuario
+  //setTimeout(function() { esperar(); }, 0100);
+  function esperar() {
+  	if (acesso==2) {
+  	console.log("o acesso está com nota"+acesso);
+  	res.send(nome+"<br/>"+senha+acesso+"<br/>"+"!!!!!!")
+  	}
+  	if (acesso==0) {
+  	setTimeout(function() { esperar(); }, 0001);
+  	}
+  	if (acesso==1) {
+  	console.log("o acesso está com nota"+acesso);
+  	res.send("não cadastrado")
+  	}
+   }
+   esperar() 
+  //espera a função buscaUsuario
+  
 })
 
 app.get('/cadastro', (req, res) => {
@@ -107,7 +132,7 @@ app.post('/rCadastro', (req, res) => {
   registraUsuarios(nome,senha)
   
 })
-//
+// aqui vai ficar a página pos cadastro
 //app.listen(port || 3000,()=>{console.log(`Example app listening on port')})
 app.listen(port || 3000, () => {
   console.log(`servidor rodando`)
